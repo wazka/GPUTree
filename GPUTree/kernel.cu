79339b1001ec0c9d7812d7e3bf78ffb44d27f336
Albert Wolant <albertwolant@gmail.com>
2016-01-05 19:28:44 +0100

    Tests running in main

--- GPUTree/kernel.cu
+++ GPUTree/kernel.cu
@@ -1,6 +1,18 @@
+#include "Tests/Tests.cuh"
 
-int main()
+
+int main(int argc, char **argv)
 {
+	//Run tests
+	if (argc == 2 && argv[1][1] == 't' && argv[1][0] == '-')
+	{
+		InitAllTests();
+
+		//Tests code here
+
+		FinalReport();
+		return EXIT_SUCCESS;
+	}
 
     return 0;
 }
